645a2573e3eca9ccc255ede7937953740482003f
Amin sheikhi <aminsheikhi@gmail.com>
2015-09-09 23:42:46 +0430

    change monodb connection string for production

--- server/config/config.js
+++ server/config/config.js
@@ -4,12 +4,13 @@ var rootPath = path.normalize(__dirname + '/../../');
 module.exports = {
     development: {
         rootPath: rootPath,
-        db: 'mongodb://localhost:27017/eMarket',
+        //db: 'mongodb://localhost:27017/eMarket',
+        db: 'mongodb://dbUser:123456@ds051788.mongolab.com:51788/emarketdb',
         port: process.env.PORT || 3030
     },
     production: {
         rootPath: rootPath,
-        db: 'mongodb://aminsheikhi@gmail.com:am681980@proximus.modulusmongo.net:27017/miNe8jym',
+        db: 'mongodb://dbUser:123456@ds051788.mongolab.com:51788/emarketdb',
         port: process.env.PORT || 80
     }
 }
